7eee893b9ae83fc1b3f2ac976ef45758e19626dd
Derek Schaeffer <dwscha02@gmail.com>
2022-08-08 18:09:44 -0600

    render fix test 6

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,6 +11,24 @@ import os
 
 import dj_database_url
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://f719f57fd1934a4eafa36b0418cbd405@o1348685.ingest.sentry.io/6628152",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..")
 
@@ -21,9 +39,13 @@ ADMIN_URL = "admin"
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# try:
+#     SECRET_KEY = os.environ["SECRET_KEY"]
+# except Exception as e:
+#     SECRET_KEY = "MOCK SECRET"
+SECRET_KEY = os.environ.get("SECRET_KEY", default="MOCK SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = "MOCK SECRET"
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 
 ALLOWED_HOSTS = ["*"]
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -6,23 +6,6 @@ These settings add some additional debugging features unsuitable for production.
 
 from .base import *  # noqa
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://f719f57fd1934a4eafa36b0418cbd405@o1348685.ingest.sentry.io/6628152",
-    integrations=[
-        DjangoIntegration(),
-    ],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
-
 # Enable Django Debug Toolbar
 # https://django-debug-toolbar.readthedocs.io
 
